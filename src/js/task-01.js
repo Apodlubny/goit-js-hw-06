ca68e3d6b9d88b23d5d29b759e497f9528639906
Apodlubny <apodlubny@gmail.com>
2022-07-23 20:13:03 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,11 @@
-const quantityOfCategories = document.querySelector("#categories");
-console.log("Number of categories:", quantityOfCategories.childElementCount);
-
-const arrayOfElements = [...quantityOfCategories.children];
-arrayOfElements.forEach((element) => {
-  console.log("Category:", element.children[0].textContent);
-  console.log("Elements:", element.children[1].childElementCount);
-  //   console.log(`Categories: ${element.querySelector("h2").textContent}
-  //     Elements: ${element.querySelectorAll("li").length}`);
+const quantityOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", quantityOfCategories.length);
+quantityOfCategories.forEach((element) => {
+  console.log(`Categories: ${element.querySelector("h2").textContent}
+      Elements: ${element.querySelectorAll("li").length}`);
 });
+// const arrayOfElements = [...quantityOfCategories.children];
+// arrayOfElements.forEach((element) => {
+// console.log("Category:", element.children[0].textContent);
+// console.log("Elements:", element.children[1].childElementCount);
+// });
